1362f4edd62524a641f18ef5f03b0c38d9f76866
evienjj <36695007+evienjj@users.noreply.github.com>
2018-02-21 18:17:47 +0700

    Update Config.java

--- src/main/java/co/moosic/music/Config.java
+++ src/main/java/co/moosic/music/Config.java
@@ -4,14 +4,14 @@ import com.kaaz.configuration.ConfigurationOption;
 
 public class Config {
     @ConfigurationOption
-    public static String discord_token = "your-discord-token";
+    public static String discord_token = "NDE1ODE3MTQ2Nzk5NjIwMDk2.DW7bbg.9A9Kdbd9IqgR0kZ7QB3P611RRCo";
 
     @ConfigurationOption
-    public static String command_prefix = "!";
+    public static String command_prefix = "!!";
 
     @ConfigurationOption
     public static int volume = 30;
 
     @ConfigurationOption
-    public static String voice_channel_id = "channel-id";
+    public static String voice_channel_id = "415812187978858497";
 }
